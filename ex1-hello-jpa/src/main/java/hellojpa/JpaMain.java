217b34ed86847c354f8c119312e1eee71355977a
jisub-dev <kimjiseob1209@gmail.com>
2026-06-28 08:31:08 +0900

    Initial commit: JPA 공부 프로젝트 (ex1-hello-jpa, jpashop) + H2

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,36 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("C");
+            System.out.println(" ===== ");
+
+            em.persist(member);
+            System.out.println("member.getId() = " + member.getId());
+            System.out.println(" ===== ");
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
